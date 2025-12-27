2d1652dea6c96702b55d98dbad0cd14d245d3313
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-12-27 21:11:02 +0600

    Create encapsulation.py

---
+++ encapsulation/encapsulation.py
@@ -0,0 +1,28 @@
+class UserAccount:
+    def __init__(self,username,email,passw):
+        self.__password=passw
+        self.__email=email
+        self.__username=username
+        self._logged_in=False
+    def login(self, passw):
+        if self.__password==passw:
+            self._logged_in=True
+        else:
+            print("wrong password")
+    def logout(self):
+        self._logged_in=False
+    def changePassword(oldpass, newpass):
+        if self._logged_in and self.__password==oldpass:
+            self.__password=newpass
+        else:
+            print("Wrong pass or log in first")
+
+    def getmaskingemail(self):
+        if self._logged_in:
+            name, domain= self.__email.split('@')
+            return name[0]+"****"+name[-1]+"@"+domain
+    def getProfile(self):
+        if self._logged_in:
+            return {"username": self.__username, "email":self.__email}
+        else:
+            print("Login first")
