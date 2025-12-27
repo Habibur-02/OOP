77e637f0169917170b7f95eb3f1e76141901b0da
Md. Habibur Rahman <156560375+Habibur-02@users.noreply.github.com>
2025-12-28 00:08:32 +0600

    Update simple inheritance.py

--- inheritance/simple inheritance.py
+++ inheritance/simple inheritance.py
@@ -1,3 +1,50 @@
+class UserAccount:
+    def __init__(self,username,email,passw):
+        self._password=passw
+        self._email=email
+        self._username=username
+        self._logged_in=False
+    def login(self, passw):
+        if self._password==passw:
+            self._logged_in=True
+        else:
+            print("wrong password")
+    def logout(self):
+        if self._logged_in==True:
+            self._logged_in=False
+            print("logged out")
+        else:
+            print("already logged out")
+
+    def changePassword(self, oldpass, newpass):
+        if self._logged_in and self._password==oldpass:
+            self._password=newpass
+        else:
+            print("Wrong pass or log in first")
+
+    def getmaskingemail(self):
+        if self._logged_in:
+            name, domain= self._email.split('@')
+            return name[0]+"****"+name[-1]+"@"+domain
+    def getProfile(self):
+        if self._logged_in:
+            name, domain= self._email.split('@')
+            mail=name[0]+"****"+name[-1]+"@"+domain
+            return {"username": self._username, "email":mail}
+        else:
+            print("Login first")
+
+
+account1=UserAccount("Habibur", "hraasif@gmail.com", "1234")
+account1.login("1234")
+account1.getProfile()
+account1.getmaskingemail()
+account1.logout()
+account1.getProfile()
+
+
+
+
 class Admin(UserAccount):
     def __init__(self, username, email, passw):
         # Initialize the parent class (UserAccount)
